524f9ad98c4e32428dadafa7425f3529ce9b3a79
Brandon Rose <rose.brandon.m@gmail.com>
2021-05-04 11:45:09 -0500

    removed unused endpoints

--- api/server.py
+++ api/server.py
@@ -16,9 +16,9 @@ logger = logging.getLogger(__name__)
 api = FastAPI(docs_url="/")
 api.include_router(models.router, tags=['Models'])
 api.include_router(dojo.router, tags=['Dojo'])
-# api.include_router(cubes.router, tags=['Cubes'])
 api.include_router(runs.router, tags=['Runs'])
-api.include_router(experiments.router, tags=['Experiments'])
+# api.include_router(cubes.router, tags=['Cubes'])
+# api.include_router(experiments.router, tags=['Experiments'])
 
 logging.basicConfig(level=logging.INFO)
 
