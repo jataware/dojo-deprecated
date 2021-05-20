bf7064d8add68bd27015845f3a82849a149ab288
Travis Hartman <63281380+travis-jat@users.noreply.github.com>
2021-05-20 11:06:27 -0700

    fixed indent

--- indicators/to_s3.py
+++ indicators/to_s3.py
@@ -17,7 +17,7 @@ export AWS_SECRET_KEY=SECRET_HERE
 def to_s3(file, bucket_name, s3_key, s3_accessKey, s3_secretKey):
     #isCM flag to upload causemosified files...differnt file structure tan tar.gz
 
-	session = boto3.Session(aws_access_key_id=s3_accessKey,aws_secret_access_key=s3_secretKey)
+    session = boto3.Session(aws_access_key_id=s3_accessKey,aws_secret_access_key=s3_secretKey)
     s3 = session.resource("s3")
     s3_client = session.client("s3")
     bucket = s3.Bucket(bucket_name)
