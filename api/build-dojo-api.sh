decfa2102d4897f9e8ae79da53a8e9ae4ac89863
Scott Haleen <scott.haleen@gmail.com>
2021-06-17 15:42:38 -0400

    docker image build script for api

---
+++ api/build-dojo-api.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+DT=$(date +"%Y%m%d")
+GIT=${DT}.git.$(git rev-parse --short HEAD)
+VERSION="0.1.0"
+TAG="api_${VERSION}"
+
+GROUP=jataware
+NAME=dojo
+IMAGE="${GROUP}/${NAME}"
+
+docker build -f Dockerfile \
+       -t "${IMAGE}:dev" \
+       -t "${IMAGE}:${TAG}" \
+       -t "${IMAGE}:${TAG}-dev" \
+       -t "${IMAGE}:${GIT}" \
+       .
