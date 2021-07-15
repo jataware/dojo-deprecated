b0201e53ecc4205df8afbf3de4ac26c2d00138e9
travis@jataware.com <travis@jataware.com>
2021-07-14 18:27:45 -0700

    add google trends files

---
+++ models/googletrends/google_trends.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+import requests
+import json
+
+url = "http://localhost:8000"
+headers = {"Content-Type": "application/json"}
+
+#### Create Model
+payload = open("google_trends.json").read()
+resp = requests.post(f"{url}/models", data=payload)
+print(resp.text)
+
+##### Add Directive
+directive = {
+  "id": "dojo/shorthand_templates/ca83b434-0f6a-4375-874c-bed9032bf779/f07f739babe071f4ef8c250865d6194c.template.txt",
+  "model_id": "ca83b434-0f6a-4375-874c-bed9032bf779",
+  "command": "python3 trends.py --term=\"{{ search_term }}\" --country=\"{{ country }}\" --state=\"{{ admin }}\" --output=output/output.csv",
+  "output_directory": "/output"
+}
+resp = requests.post(f"{url}/dojo/directive", json=directive)
+print(resp.text)
+
+##### Add OutputFile 1
+mapper = json.loads(open("google_trends_mapper.json").read())
+outputfile = {
+    "id": "google_trends-outputfile-1",
+    "model_id": "ca83b434-0f6a-4375-874c-bed9032bf779",
+    "name": "google_trends",
+    "file_type": "csv",
+    "output_directory": "/",
+    "path": "output.csv",
+    "transform": mapper,
+}
+resp = requests.post(f"{url}/dojo/outputfile", json=[outputfile])
+print(resp.text)
